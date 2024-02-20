a710404054e28b36d1f906e8a634b164906f6689
Andrei Nesterov <35880308+AndreiNesterov@users.noreply.github.com>
2024-02-20 23:26:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    readme_contents = fh.read()
+
 setup(
     name='LODlit',
-    version='0.5.0',    
+    version='0.6.0',    
     description = "Retrieving literal values from LOD",
+    long_description=readme_contents,
+    long_description_content_type='text/markdown',
     author = "Andrei Nesterov",
     author_email = "nesterov@cwi.nl",
-    license = "CC BY 4.0",
+    license = "CC BY SA 4.0",
     keywords = ["LOD", "literals", "linked open data", "strings", "NLP"],
-    url='https://github.com/cultural-ai/LODlit/tree/main/LODlit_package',
+    url='https://github.com/cultural-ai/LODlit',
     install_requires=[
         'nltk==3.8.1',
 	    'pandas==2.0.3',
